cbf7586ad5424b26ff83658012ef19136eac0e51
tianijessie2-prog <tianijessie2@gmail.com>
2025-10-31 14:45:24 +0100

    Portolio Propre de FKBF

--- src/pages/MyProject.tsx
+++ src/pages/MyProject.tsx
@@ -2,18 +2,34 @@ import "./MyProject.css";
 import { Github, ChevronLeft, ChevronRight } from "lucide-react";
 import { useState } from "react";
 
+type DevProject = {
+  title: string;
+  description: string;
+  image: string;
+  tech: string[];
+  github: string;
+  category: string;
+};
+
+type GraphicProject = {
+  title: string;
+  description: string;
+  image: string;
+  tools: string[];
+};
+
 function MyProject() {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
   const [currentTab, setCurrentTab] = useState<"dev" | "graphic">("dev");
 
-  const devProjects = [
+  const devProjects: DevProject[] = [
     {
       title: "Dashboard de FCRV Bank",
       description:
         "Il s'agit d'une application web de gestion dans une banque car de FCRV Bank",
       image: "/bank.png",
       tech: ["HTML 5", "CSS 3", "JavaScript"],
-      github: "https://github.com/FoukengFranck/FRCVBank.git",
+      github: "https://github.com/user/food-delivery",
       category: "Dashboard de Bank",
     },
     {
@@ -22,16 +38,16 @@ function MyProject() {
         "Il s'agit d'une plateforme e-commerce (En ce qui concerne les telephone de la marque Iphone) pour acheter des Télephone et Accesoires Iphone variés en ligne.",
       image: "/fkbfstore.png",
       tech: ["HTML 5", "CSS 3", "JS"],
-      github: "https://github.com/FoukengFranck/fkbf-store.git",
+      github: "https://github.com/user/penny-mall",
       category: "E-commerce Platform",
     },
     {
       title: "Calculatrice Simple",
       description:
-        "Realisation d'une calculatrice simple utilisant HTML et CSS pour la structure et le style et PHP pour effectuer le calcul.",
+        "Réalisation d'une calculatrice simple utilisant HTML et CSS pour la structure et le style, et PHP pour effectuer le calcul.",
       image: "/calculatrice.png",
       tech: ["HTML", "CSS", "PHP"],
-      github: "https://github.com/FoukengFranck/Calculatrice.git",
+      github: "https://github.com/user/muhammad-portfolio",
       category: "Simple Calculator",
     },
     {
@@ -40,15 +56,15 @@ function MyProject() {
         "Application web qui permet de générer des avatars d'alpaca personnalisés en utilisant différentes options de personnalisation.",
       image: "/avatar.png",
       tech: ["HTML", "CSS", "JS"],
-      github: "https://github.com/FoukengFranck/g-n-rateur-d-avatar.git",
-      category: "YouTube Channel Site",
+      github: "https://github.com/user/youtuber-site",
+      category: "Avatar Generator",
     },
     {
       title: "My Portfolio Website",
       description: "Site vitrine présentant mes projets et compétences.",
       image: "/portfolio.png",
       tech: ["React", "TailwindCSS"],
-      github: "https://github.com/FoukengFranck/mon-portfolio.git",
+      github: "https://github.com/user/luxury-offers",
       category: "Portfolio",
     },
     {
@@ -57,53 +73,46 @@ function MyProject() {
         "Application web pour la gestion et le suivi des dossiers médicaux des patients.",
       image: "/suiviemedicale.png",
       tech: ["HTML", "TailwindCSS", "JavaScript"],
-      github: "https://github.com/FoukengFranck/medilink.git",
+      github: "https://github.com/user/hotel-booking",
       category: "Medical Tracking System",
     },
   ];
 
-  const graphicProjects = [
+  const graphicProjects: GraphicProject[] = [
     {
       title: "Logo Design pour FCRV Bank",
-      description:
-        "Conception d'un logo moderne et épuré pour une banque, intégrant des éléments symbolisant la finance et la confiance.",
+      description: "Logo moderne pour une banque, symbole de confiance.",
       image: "/logofcrv.jpg",
       tools: ["Photoshop", "Illustrator"],
-      category: "Logo Design",
     },
     {
       title: "Affiche BURGER",
       description: "Affiche publicitaire dynamique pour un restaurant.",
       image: "/Flyers.png",
       tools: ["Photoshop"],
-      category: "Promotional Poster",
     },
     {
       title: "Flyers Boutique Girly Vibes",
       description: "Création d’un flyer attrayant pour une boutique de mode.",
       image: "/GIRLY VIBES SHOP1.png",
       tools: ["Canva"],
-      category: "Digital Illustration",
     },
     {
       title: "Carte de Visite",
       description: "Identité visuelle complète avec carte de visite.",
       image: "/carte de visite.png",
       tools: ["Photoshop", "Canva"],
-      category: "Visual Identity",
     },
     {
       title: "Flyers Menu",
       description: "Menu professionnel et appétissant pour un restaurant.",
       image: "/MENU.jpg",
       tools: ["Photoshop"],
-      category: "Infographic",
     },
   ];
 
   const currentProjects = currentTab === "dev" ? devProjects : graphicProjects;
-  const currentTitle =
-    currentTab === "dev" ? "development projects" : "graphic design projects";
+  const isGraphic = currentTab === "graphic";
 
   const handleLeftClick = () => {
     if (currentTab === "graphic") {
@@ -117,22 +126,18 @@ function MyProject() {
     }
   };
 
-  const isGraphic = currentTab === "graphic";
-
   return (
-    <div className="container mx-auto px-4 py-12 max-w-7xl">
+    <div className="my-projects-container">
       <div className="text-center mb-9">
         <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-white mb-4">
           My <span className="text-purple-500">Projects</span>
         </h1>
         <div className="w-24 h-1 bg-purple-500 mx-auto rounded-full"></div>
       </div>
-      <div className="text-center mb-10">
-        <h3 className="text-lg sm:text-2xl md:text-3xl font-bold text-white mb-4">
-          <span className="text-purple-500">my</span> {currentTitle}
-        </h3>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 mb-12">
+      <h2 className="text-3xl font-bold text-center mb-8 text-purple-700">
+        {isGraphic ? "Graphic Design Projects" : "Development Projects"}
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-12">
         {currentProjects.map((project, index) => (
           <div
             key={index}
@@ -165,22 +170,26 @@ function MyProject() {
                 </p>
               )}
               <div className="flex flex-wrap gap-2 mb-4">
-                {(isGraphic ? project.tools : project.tech)?.map(
-                  (tag, tagIndex) => (
-                    <span
-                      key={tagIndex}
-                      className="bg-purple-100 text-purple-800 px-2 py-1 rounded-full text-xs font-semibold"
-                    >
-                      {tag}
-                    </span>
-                  )
-                )}
+                {"tools" in project
+                  ? project.tools.map((tag, tagIndex) => (
+                      <span
+                        key={tagIndex}
+                        className="bg-purple-100 text-purple-800 px-2 py-1 rounded-full text-xs font-semibold"
+                      >
+                        {tag}
+                      </span>
+                    ))
+                  : project.tech.map((tag, tagIndex) => (
+                      <span
+                        key={tagIndex}
+                        className="bg-purple-100 text-purple-800 px-2 py-1 rounded-full text-xs font-semibold"
+                      >
+                        {tag}
+                      </span>
+                    ))}
               </div>
               <div className="flex justify-between items-center">
-                <span className="text-purple-600 font-semibold text-sm">
-                  {project.category}
-                </span>
-                {!isGraphic && project.github && (
+                {"github" in project && project.github && (
                   <a
                     href={project.github}
                     target="_blank"
@@ -195,24 +204,24 @@ function MyProject() {
             </div>
           </div>
         ))}
-        {selectedImage && (
-          <div className="fixed inset-0 bg-black/80 flex justify-center items-center z-50 transition-opacity duration-300 animate-fadeIn">
-            <div className="relative max-w-4xl w-full p-4">
-              <button
-                onClick={() => setSelectedImage(null)}
-                className="absolute top-4 right-4 text-white text-3xl font-bold hover:text-purple-400 transition"
-              >
-                &times;
-              </button>
-              <img
-                src={selectedImage}
-                alt="Aperçu"
-                className="w-full max-h-[80vh] object-contain rounded-lg shadow-lg"
-              />
-            </div>
-          </div>
-        )}
       </div>
+      {selectedImage && (
+        <div className="fixed inset-0 bg-black/80 flex justify-center items-center z-50 transition-opacity duration-300 animate-fadeIn">
+          <div className="relative max-w-4xl w-full p-4">
+            <button
+              onClick={() => setSelectedImage(null)}
+              className="absolute top-4 right-4 text-white text-3xl font-bold hover:text-purple-400 transition"
+            >
+              &times;
+            </button>
+            <img
+              src={selectedImage}
+              alt="Aperçu"
+              className="w-full max-h-[80vh] object-contain rounded-lg shadow-lg"
+            />
+          </div>
+        </div>
+      )}
       <div className="flex justify-center items-center space-x-4 mb-8">
         <button
           onClick={handleLeftClick}
